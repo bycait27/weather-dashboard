123a0a01f360b0aae8ba1d6417661ae51af44ce0
Caitlin Ash <caitlinba27@gmail.com>
2023-08-03 18:52:26 -0500

    Attempted to fetch weather api and display results

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const searchBtn = document.getElementById("search-btn");
+
+const weather = {
+    Atlanta: {
+        lat: "33.753746",
+        lon: "-84.386330",
+    }
+};
+
+// console.log(`Lat: ${weather.Atlanta.lat}`);
+
+console.log(weather.Atlanta.lat);
+
+
+function fetchData() {
+    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${weather.Atlanta.lat}&lon=${weather.Atlanta.lon}&appid=5dec7b6debd352b0f0b1900bde30e10b`)
+    .then(function(response) {
+        return response.json();
+    })
+
+
+.then(function(data) {
+    console.log(data);
+    let forecastElement = document.getElementById("forecast");
+    forecastElement.innerHTML = "";
+});
+};
+
+searchBtn.addEventListener("click", fetchData);
