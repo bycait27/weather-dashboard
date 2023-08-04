2737eb42317ac1d62085022c610629f72736d650
Caitlin Ash <caitlinba27@gmail.com>
2023-08-03 21:41:03 -0500

    Attempted to fix fetch again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,39 @@
+// stores user's search bar input
+const userInput = document.getElementById("city-search");
+// stores search button
 const searchBtn = document.getElementById("search-btn");
+// stores user's input fro search bar into a city parameter
+let city = userInput.value;
+// stores api key for weather api
+const apiKey = "5dec7b6debd352b0f0b1900bde30e10b";
 
-const weather = {
-    Atlanta: {
-        lat: "33.753746",
-        lon: "-84.386330",
-    }
-};
+// stores url for weather api
+const queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=apiKey";
 
-// console.log(`Lat: ${weather.Atlanta.lat}`);
+// function for fetching weather
+function getForecast(city) {
+    let response = fetch(queryUrl);
+    response.json();
+}
 
-console.log(weather.Atlanta.lat);
-
-
-function fetchData() {
-    fetch(`api.openweathermap.org/data/2.5/forecast?lat=${weather.Atlanta.lat}&lon=${weather.Atlanta.lon}&appid={API KEY}`)
-    .then(function(response) {
-        return response.json();
-    })
-
-
-.then(function(data) {
-    console.log(data);
+searchBtn.addEventListener("click", function(event) {
+    getForecast(userInput.value);
     let forecastElement = document.getElementById("forecast");
-    forecastElement.innerHTML = "";
+    forecastElement.innerHTML = getForecast(userInput.value)
 });
-};
 
-searchBtn.addEventListener("click", fetchData);
+// function fetchData() {
+//     fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=apiKey`)
+//     .then(function(response) {
+//         return response.json();
+//     })
+
+
+// .then(function(data) {
+//     console.log(data);
+//     let forecastElement = document.getElementById("forecast");
+//     forecastElement.innerHTML = "";
+// });
+// };
+
+// searchBtn.addEventListener("click", fetch(queryUrl));
